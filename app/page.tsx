42ae53cf8271d1bd14ef544fb557571e7c9ed6f8
yaserbc <legnawings@gmail.com>
2024-06-08 19:02:56 +1000

    go

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Hello from Yaser!
@@ -29,7 +29,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className={styles.center}>
+      <div>
        <p>
      <br/> ANSWER 1:
      </p>
